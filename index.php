80f20873cc4082a15803862e981bbe868d09a10d
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-01-28 22:44:28 -0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+    ChocolateyCMS | Public BETA.
+
+    Chocolatey  Copyright (C) 2017  Claudio Santoro
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.
+*/
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+*/
+
+$app = require __DIR__ . '/bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
+
+$app->run();
