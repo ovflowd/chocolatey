ac6078804739c0f3e285c5debf437cc14de32e7f
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-01-29 00:51:21 -0200

    Create Generators.php

---
+++ app/Facades/Generators.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * Class Generators
+ * @package App\Facades
+ */
+class Generators extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'azuregenerators';
+    }
+}
