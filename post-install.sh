affde77ac7617998dd873bcd6f5e241dae76812f
sant0ro <claudio.santoro@redes.unb.br>
2017-02-25 15:49:14 -0300

    removed shit

--- post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-find ./vendor -name ".git" -exec rm -rf {} \;
-find ./vendor -name ".gitignore" -exec rm {} \;
-find ./vendor -name "*.md" -exec rm -rf {} \;
-
-echo "Removing Useless Files"
