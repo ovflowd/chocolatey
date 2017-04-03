426a03bd641ed8f4a4c521d81a9247c76777006c
Claudio Santoro <claudio.santoro@ieee.org>
2017-04-03 18:45:58 -0300

    Continued Template Engine

--- resources/lang/en/playing_habbo/how_to_play.php
+++ resources/lang/en/playing_habbo/how_to_play.php
@@ -1,5 +1,32 @@
 <?php
 
-return [
+/**
+ * Chocolatey Language Template
+ *
+ * @template How to Play Playing Habbo Page
+ * @uri /playing_habbo/how_to_play.en.html
+ */
 
+return [
+    // Headings
+    'title' => 'How to play',
+    'have' => 'You&apos;ve styled your avatar, gotten comfy in your homeroom and you&apos;ve been shown how a few things work by the Hotel Manager&#x2026; So, what next?',
+    // Ideas Section
+    'ideas' => 'Here are some ideas:',
+    'explore_title' => 'Explore rooms',
+    'explore' => 'Click the Navigator and choose one of the public rooms where you can chat with other :hotelNames.',
+    'friends_title' => 'Make friends',
+    'friends' => 'Click on a :hotelName , ask them to be your friend or give them respect!',
+    'citizenship_title' => 'Complete your :hotelName Citizenship',
+    'citizenship' => 'To get to know the Hotel a bit better, start by clicking on the Help tool in the the top right corner in the hotel.',
+    'rooms_title' => 'Visit game rooms',
+    'rooms' => 'Find the Game Hub in the list of public rooms in the navigator. Once there, use any of the arcade machines to go to a game room!',
+    'shop_title' => '>Go shopping',
+    'shop' => 'Go to the Duckets shop and see what your free duckets can get you!',
+    'activities_title' => 'Check out the latest activities',
+    'activities' => 'Visit the <a href="/">Home</a> section of the website to find out all the latest news, competitions and general goings-on in :hotelName',
+    'activities_two' => 'Once you&apos;ve done a few of these, you will be well on your way to becoming a fully fledged :hotelName citizen!',
+    // Footer Section
+    'join_title' => 'Join :hotelName',
+    'join' => 'Haven&apos;t registered for :hotelName yet? <a href="/registration">Join now</a>!'
 ];
