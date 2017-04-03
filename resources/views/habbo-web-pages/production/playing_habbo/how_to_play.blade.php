82e2afc1400645c6544899d8ad40f3b9016da1db
Claudio Santoro <claudio.santoro@ieee.org>
2017-04-03 18:49:35 -0300

    Continued Template Engine

--- resources/views/habbo-web-pages/production/playing_habbo/how_to_play.blade.php
+++ resources/views/habbo-web-pages/production/playing_habbo/how_to_play.blade.php
@@ -1,40 +1,40 @@
-<h1>How to play</h1>
-<p>You&apos;ve styled your avatar, gotten comfy in your homeroom and you&apos;ve been shown how a few things work by the Hotel Manager&#x2026; So, what next?</p>
-<p>Here are some ideas:</p>
+<h1>{!! trans('playing_habbo/how_to_play.title') !!}</h1>
+<p>{!! trans('playing_habbo/how_to_play.have') !!}</p>
+<p>{!! trans('playing_habbo/how_to_play.ideas') !!}</p>
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/navigator.png" alt="Navigator" class="align-right">
 </p>
-<h3>Explore rooms</h3>
-<p>Click the Navigator and choose one of the public rooms where you can chat with other {{$chocolatey->hotelName}}s.</p>
+<h3>{!! trans('playing_habbo/how_to_play.explore_title') !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.explore', ['hotelName' => $chocolatey->hotelName]) !!}</p>
 <hr>
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/askfriend.png" alt="Ask to be friend" class="align-right">
 </p>
-<h3>Make friends</h3>
-<p>Click on a {{$chocolatey->hotelName}} , ask them to be your friend or give them respect!</p>
+<h3>{!! trans('playing_habbo/how_to_play.friends_title') !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.friends', ['hotelName' => $chocolatey->hotelName]) !!}</p>
 <hr>
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/citizenship.png" alt=" {{$chocolatey->hotelName}} citizenship" class="align-right">
 </p>
-<h3>Complete your {{$chocolatey->hotelName}} Citizenship</h3>
-<p>To get to know the Hotel a bit better, start by clicking on the Help tool in the the top right corner in the hotel.</p>
+<h3>{!! trans('playing_habbo/how_to_play.citizenship_title', ['hotelName' => $chocolatey->hotelName]) !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.citizenship') !!}</p>
 <hr>
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/gamehub.png" alt="Game Hub" class="align-right">
 </p>
-<h3>Visit game rooms</h3>
-<p>Find the Game Hub in the list of public rooms in the navigator. Once there, use any of the arcade machines to go to a game room!</p>
+<h3>{!! trans('playing_habbo/how_to_play.rooms_title') !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.rooms') !!}</p>
 <hr>
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/shop.png" alt="Shop" class="align-right"/>
 </p>
-<h3>Go shopping</h3>
-<p>Go to the Duckets shop and see what your free duckets can get you!</p>
+<h3>{!! trans('playing_habbo/how_to_play.shop_title') !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.shop') !!}</p>
 <hr>
-<h3>Check out the latest activities</h3>
-<p>Visit the <a href="/">Home</a> section of the website to find out all the latest news, competitions and general goings-on in {{$chocolatey->hotelName}}</p>
-<p>Once you&apos;ve done a few of these, you will be well on your way to becoming a fully fledged {{$chocolatey->hotelName}} citizen!</p>
+<h3>{!! trans('playing_habbo/how_to_play.activities_title') !!}</h3>
+<p>{!! trans('playing_habbo/how_to_play.activities', ['hotelName' => $chocolatey->hotelName]) !!}</p>
+<p>{!! trans('playing_habbo/how_to_play.activities_two', ['hotelName' => $chocolatey->hotelName]) !!}</p>
 <blockquote>
-    <h3>Join {{$chocolatey->hotelName}} </h3>
-    <p>Haven&apos;t registered for {{$chocolatey->hotelName}} yet? <a href="/registration">Join now</a>!</p>
+    <h3>{!! trans('playing_habbo/how_to_play.join_title', ['hotelName' => $chocolatey->hotelName]) !!}</h3>
+    <p>{!! trans('playing_habbo/how_to_play.join', ['hotelName' => $chocolatey->hotelName]) !!}</p>
 </blockquote>
