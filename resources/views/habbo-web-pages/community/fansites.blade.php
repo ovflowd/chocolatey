dae9492f09086f7c0a6e1a9a437893e7bd6554d7
BobyTS <contact@bobbaworld.fr>
2017-04-08 03:25:23 +0200

    Some translation corrections on the Fansites page

--- resources/views/habbo-web-pages/community/fansites.blade.php
+++ resources/views/habbo-web-pages/community/fansites.blade.php
@@ -2,7 +2,7 @@
 <p>
     <img src="{{$chocolatey->hotelUrl}}habbo-web/assets/web-images/habbo_friends.png" alt="Fansites" class="align-right">
 </p>
-<p>{!! trans('community/fansites.heading') !!}</p>
+<p>{!! trans('community/fansites.heading',['hotelName' => $chocolatey->hotelName]) !!}</p>
 <ul>
     <li>
         <a href="http://ragezone.com/" target="_blank">RaGEZONE</a>
