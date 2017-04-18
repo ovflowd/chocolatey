50b6046b911686ca30007ff4e72b181b8f044361
Claudio Santoro <claudio.santoro@ieee.org>
2017-04-18 18:21:54 -0300

    Updates

--- database/migrations/2017_04_18_203722_move_users_to_chocolatey.php
+++ database/migrations/2017_04_18_203722_move_users_to_chocolatey.php
@@ -1,9 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
 
 class MoveUsersToChocolatey extends Migration
 {
@@ -14,9 +12,7 @@ class MoveUsersToChocolatey extends Migration
      */
     public function up()
     {
-        Schema::table('chocolatey_users_id', function (Blueprint $table) {
-            DB::raw('UPDATE chocolatey_users_id chocoUser INNER JOIN users arcturusUser ON chocoUser.mail = arcturusUser.mail SET chocoUser.password = users.password');
-        });
+        DB::raw('UPDATE chocolatey_users_id chocoUser INNER JOIN users arcturusUser ON chocoUser.mail = arcturusUser.mail SET chocoUser.password = users.password');
     }
 
     /**
