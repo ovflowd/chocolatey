d27b58a0653fba3a3664de9fd8463e766c72cf06
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-04-20 13:20:13 +0000

    Apply fixes from StyleCI

--- app/Helpers/User.php
+++ app/Helpers/User.php
@@ -75,8 +75,9 @@ final class User extends Singleton
     {
         $chocolateyId = ChocolateyId::find($request->json()->get('email'));
 
-        if ($chocolateyId == null)
-            return null;
+        if ($chocolateyId == null) {
+            return;
+        }
 
         $user = $chocolateyId->last_logged_id == 0 ? UserModel::where('mail', $request->json()->get('email'))->first() :
             UserModel::find($chocolateyId->last_logged_id);
