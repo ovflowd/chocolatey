a82145b28e42b206689483cc73e1de4091d3d526
Claudio Santoro <claudio.santoro@ieee.org>
2017-04-23 16:22:37 -0300

    Updating

--- tests/ApiTest.php
+++ tests/ApiTest.php
@@ -1,22 +1,34 @@
 <?php
 
+use Illuminate\Support\Facades\DB;
+use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\TestCase;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 /**
  * Class ApiTest.
  */
 class ApiTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /**
+     * Test Index of the API
+     *
      * @path /api/
      * @test
      */
     public function testIndex()
     {
         $this->get('/api')->seeStatusCode(401);
+    }
+
+    public function testRooms()
+    {
+        $this->get('api/public/rooms/0')->seeJson([
+            'error' => 'not-found']);
     }
 
     /**
@@ -28,6 +40,18 @@ class ApiTest extends TestCase
      */
     public function createApplication()
     {
-        return require __DIR__.'/../bootstrap/app.php';
+        return require __DIR__ . '/../bootstrap/app.php';
+    }
+
+    /**
+     * Setup the test environment.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+
+        DB::unprepared(file_get_contents(__DIR__ . '/../database/arcturus/arcturus.sql'));
     }
 }
