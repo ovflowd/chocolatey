b1c651a3c983c2dbc1c013b9ad1728d5c87dfb55
Claudio Santoro <claudio.santoro@ieee.org>
2017-05-02 10:09:42 -0300

    Some Fixes

--- resources/lang/nl/common/box_how_to_play.php
+++ resources/lang/nl/common/box_how_to_play.php
@@ -4,11 +4,11 @@
  * Chocolatey Language Template.
  *
  * @template How to Play Habbo Common Box
- * @uri /common/box_how_to_play.en.html
+ * @uri /common/box_how_to_play.nl.html
  * @language nl (dutch)
  */
 
 return [
     'title'       => 'Hoe kan je spelen', // Template Title
-    'description' => 'Word creatief, word een bouwer, word een socializer! Bekijk onze <a href="/playing-habbo/how-to-play">tips over wat je in :hotelName kunt doen</a>.', // Template Content
+    'description' => 'Word creatief, word een bouwer, word een socializer! Bekijk onze <a href="/playing-habbo/how-to-play">tips over wat je in :hotelName kunt doen</a>.' // Template Content
 ];
