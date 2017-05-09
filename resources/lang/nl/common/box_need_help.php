dcc96d0547aa3cd48402b576f17252570d603303
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-05-09 18:07:31 +0000

    Apply fixes from StyleCI

--- resources/lang/nl/common/box_need_help.php
+++ resources/lang/nl/common/box_need_help.php
@@ -11,5 +11,5 @@
 return [
     'title'   => 'Hulp nodig?', // Template Title
     'learn'   => 'Bekijk hoe je jezelf kunt helpen of hoe je hulp kunt inschakelen <a href="/playing-habbo/help">Hulp pagina</a>. Als je behoefte hebt om met iemand te praten kun je hier ook een lijst vinden met telefoonnummers en websites.', // How to Learn
-    'answers' => 'Kun je hier de antwoorden niet vinden, bekijk dan onze Klantenservice &amp; Helptool paginas.' // Being Safe
+    'answers' => 'Kun je hier de antwoorden niet vinden, bekijk dan onze Klantenservice &amp; Helptool paginas.', // Being Safe
 ];
