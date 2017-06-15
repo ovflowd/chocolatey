6de5ef4f6a6a5c4c7332788326025b5f3564f736
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-06-15 15:59:45 -0300

    Updated Bash

--- post-install.sh
+++ post-install.sh
@@ -1,7 +1,14 @@
+#!/usr/bin/env bash
 #/bin/bash
 
 find ./vendor -name ".git" -exec rm -rf {} \; 2>/dev/null
 find ./vendor -name ".gitignore" -exec rm {} \; 2>/dev/null
 find ./vendor -name "*.md" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "*LICENSE.txt" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "LICENSE" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "*.yml" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "docs/*" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "tests/*" -exec rm -rf {} \; 2>/dev/null
+find ./vendor -name "test/*" -exec rm -rf {} \; 2>/dev/null
 
 echo "Removing Useless Files"
