cc366381c2f3ceef26b0dd001c516a3fbdbf115a
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-07-29 18:35:42 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- resources/lang/fr/common/staff_about.php
+++ resources/lang/fr/common/staff_about.php
@@ -9,7 +9,7 @@
  */
 
 return [
-    'title' => 'About Staff',
+    'title'       => 'About Staff',
     'description' => '<img src=":hotelUrl/habbo-web/assets/images/ADM.gif"> <b>:hotelName Staff </b> c\'est une équipe d\'experts créée'
-        . 'Pour gérer: hotelName Hôtel avec une expertise et une excellente connaissance. <br><br> Si vous avez des questions. Demandez à l\'équipe du personnel de vous aider.'
+        .'Pour gérer: hotelName Hôtel avec une expertise et une excellente connaissance. <br><br> Si vous avez des questions. Demandez à l\'équipe du personnel de vous aider.',
 ];
